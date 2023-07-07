ac277d53b31e7f16d7eae2bf21f83139c266e2d6
William Chen <wichen@sram.com>
2023-07-08 00:56:16 +0800

    remove unused code in server.py

--- auth/server.py
+++ auth/server.py
@@ -36,39 +36,6 @@ def register():
     server_print(f"Saving to db with {email}, {user_name}")
     return validation_json, 200
 
-# @app.route("/login", methods= ["POST"])
-# def login():
-#     sid = request.cookies.get("sid")
-#     if sid is not None and session_store.is_valid_sid(sid):
-#         session_store.refresh_session(sid)
-#         user_dict = session_store.get_user_dict_from_session(sid)
-#         current_user = User(**user_dict)
-#         login_response =  LoginResponse(user=current_user, is_sid=True)
-#         server_print(f"User already logged in with sid: {sid}")
-#         return login_response.to_dict(), 200
-
-#     user_info: dict[str, str] = request.get_json()
-#     email = user_info.get("email")
-#     pwd = user_info.get("password")
-#     if email is None or pwd is None:
-#         return {"error_message": "Missing email or password"}, 400
-
-    
-#     login_response = LoginManager(email, pwd).login()
-#     if login_response.is_error():
-#         return login_response.to_dict(), 400
-    
-#     _1_month_in_seconds = 60 * 60 * 24 * 30
-#     if login_response.is_login():
-#         user_dict = login_response.user.to_dict()
-#         sid = session_store.add_user_in_session(user_dict)
-#         #set cookies for sid
-#         cookie_response = jsonify(login_response.to_dict())
-#         cookie_response.set_cookie("sid", sid, expires= _1_month_in_seconds)
-
-
-#     return cookie_response, 200
-
 @app.route("/login", methods= ["POST"])
 def login():
     cookie_sid = request.cookies.get("sid")
