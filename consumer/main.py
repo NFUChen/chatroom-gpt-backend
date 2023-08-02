38c3623b4cdf04eb33247a3d22eef7c72eaae2d4
William Chen <wichen@sram.com>
2023-08-02 21:34:44 +0800

    bugfix for consumer while inserting data, args mismatch

--- consumer/main.py
+++ consumer/main.py
@@ -6,17 +6,21 @@ from consumer import Consumer
 
 consumer = Consumer("guest", "guest", "rabbitmq")
 
-def insert_user(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+def insert_user_callback(ch, method, properties, body):
+    # print(" [x] Received %r" % body)
     message_dict = ast.literal_eval(body.decode())
     user = message_dict["data"]
-    user_email = user["uesr_email"]
+    user_email = user["user_email"]
     uesr_name = user["user_name"]
     pwd = user["password"]
-    mysqldb_manger.insert_user(user_email,user_email, uesr_name,pwd)
+    mysqldb_manger.insert_user(
+        user_email, 
+        uesr_name,
+        pwd
+    )
 
 queue_with_callbacks = (
-    ("user", insert_user),
+    ("user", insert_user_callback),
 )
 for queue, callback in queue_with_callbacks:
     consumer.consume(queue, callback)
