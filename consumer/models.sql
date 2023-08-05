496191d74d20b8d95c04b078251ec806c8813ada
William Chen <wichen@sram.com>
2023-08-05 17:40:04 +0800

    update models.sql, add gpt_responses and gpt_messages delete room and user should be soft delete (i.e., changing a attribute in a record), since all data relationship is tightly coupled

--- consumer/models.sql
+++ consumer/models.sql
@@ -3,7 +3,8 @@ CREATE TABLE
         user_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
         user_email VARCHAR(255) NOT NULL UNIQUE,
         user_name VARCHAR(255) NOT NULL,
-        password VARCHAR(72) NOT NULL
+        password VARCHAR(72) NOT NULL,
+        is_deleted BOOLEAN NOT NULL DEFAULT 0,
     );
 
 CREATE TABLE IF NOT EXISTS rooms (
@@ -11,6 +12,7 @@ CREATE TABLE IF NOT EXISTS rooms (
     owner_id INT NOT NULL,
     room_name VARCHAR(255) NOT NULL UNIQUE,
     room_type VARCHAR(10) NOT NULL,
+    is_deleted BOOLEAN NOT NULL DEFAULT 0,
     FOREIGN KEY (owner_id) REFERENCES users(user_id)
 );
 
@@ -18,8 +20,8 @@ CREATE TABLE
     IF NOT EXISTS chat_messages (
         message_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
         type VARCHAR(10) CHECK (
-            type = "regular"
-            OR type = "ai"
+            type = 'regular'
+            OR type = 'ai'
         ) NOT NULL,
         room_id INT NOT NULL,
         user_id INT NOT NULL,
@@ -28,5 +30,29 @@ CREATE TABLE
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         FOREIGN KEY (room_id) REFERENCES rooms(room_id),
-        FOREIGN KEY (user_id) REFERENCES users(user_id)
+        FOREIGN KEY (user_id) REFERENCES users(user_id),
     );
+
+CREATE TABLE 
+    IF NOT EXISTS gpt_responses (
+        response_id VARCHAR(36) PRIMARY KEY NOT NULL,
+        datetime TIMESTAMP NOT NULL,
+        answer TEXT NOT NULL,
+        prompt_tokens INT NOT NULL,
+        response_tokens INT NOT NULL,
+        room_id INT NOT NULL,
+        user_id INT NOT NULL,
+        api_key VARCHAR(255) NOT NULL,
+        FOREIGN KEY (room_id) REFERENCES rooms(room_id),
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE
+    IF NOT EXISTS gpt_messages (
+        response_id VARCHAR(36) NOT NULL,
+        role VARCHAR(10) CHECK (
+            role = 'assistant' or role = 'user'
+        ) NOT NULL,
+        content TEXT NOT NULL,
+        FOREIGN KEY (response_id) REFERENCES gpt_responses(response_id)
+);
