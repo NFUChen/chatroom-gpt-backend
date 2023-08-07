abd608eba92b2c7437c14f033a490eaff7630282
William Chen <wichen@sram.com>
2023-08-07 21:00:00 +0800

    add is_deleted attribute for User to align with MySQL table users

--- auth/user.py
+++ auth/user.py
@@ -4,11 +4,8 @@ class User:
     user_id: int
     user_email: str
     user_name: str
+    is_deleted: bool
     password: str = None
 
     def to_dict(self) -> dict[str, str]:
-        return {
-            "user_id": self.user_id,
-            "user_email": self.user_email,
-            "user_name": self.user_name,
-        } 
+        return self.__dict__
