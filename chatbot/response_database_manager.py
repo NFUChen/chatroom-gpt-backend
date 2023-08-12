5c06421a7dd32cfbce3aa506de23992d2e7ff7e6
William Chen <wichen@sram.com>
2023-08-12 21:39:25 +0800

    finish ResponseDatabaseManager and fix typo in class name

--- chatbot/response_database_manager.py
+++ chatbot/response_database_manager.py
@@ -1,3 +1,13 @@
-class ResponseDatabaseManaget:
+from typing import Any
+import requests
+class ResponseDatabaseManager:
     def __init__(self) -> None:
-        pass
+        self.produce_api =  "http://producer:5000/produce"
+
+    def save_response(self, response_dict: dict[str, Any]) -> None:
+        return requests.post(
+            self.produce_api, json= response_dict
+        )
+    
+
+response_db_manager = ResponseDatabaseManager()
