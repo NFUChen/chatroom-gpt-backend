011776e286644ad83c4692612214121c19608838
William Chen <wichen@sram.com>
2023-09-03 09:05:47 +0800

    add error detail function, and give helper error message for the case that API caller is not a login user in login_required decorator

--- chatroom-server/utils.py
+++ chatroom-server/utils.py
@@ -1,7 +1,21 @@
 import functools
 from flask import request
 import requests
+import traceback
 
+def get_error_detail(e: Exception):
+    error_name = e.__class__.__name__
+    trace_back = traceback.extract_tb(e.__traceback__)
+    file_name = trace_back[-1].filename
+    line_number = trace_back[-1].lineno
+    error_message = str(e)
+    return {
+        'error_name': error_name,
+        'file_name': file_name,
+        'line_number': line_number,
+        'error_message': error_message,
+        "trace_back": str(trace_back)
+    }
 
 def handle_server_errors(func):
     @functools.wraps(func)
@@ -14,7 +28,7 @@ def handle_server_errors(func):
         except Exception as error:
             return {
                 "data": None,
-                "error": str(error)
+                "error": get_error_detail(error)
             }, 200
     return decorated
 
@@ -34,6 +48,12 @@ def login_required(func):
             }, 401
         
         user_dict = response.json()["data"]
+        if user_dict is None:
+            return {
+                "data": None,
+                "error": "Please login first before this API call."
+            }, 401
+
         request.json["user"] = user_dict
 
 
